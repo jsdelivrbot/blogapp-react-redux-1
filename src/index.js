898248dcc3c4a4deb6da2d4c801e902d156fb733
Shelly Tang <shellytang@gmail.com>
2017-08-22 20:28:34 -0700

    removed unused app and added route for posts index

--- src/index.js
+++ src/index.js
@@ -5,25 +5,17 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 import {BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts-index';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-class Hello extends React.Component {
-  render () { return <div>Hello!</div>}
-}
-
-class Goodbye extends React.Component {
-  render () { return <div>Bye!</div>}
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <div>
-          <Route path="/hello" component={Hello}/>
-          <Route path="/goodbye" component={Goodbye}/>
+          <Route path='/'component={PostsIndex}/>
         </div>
       </BrowserRouter>
   </Provider>
