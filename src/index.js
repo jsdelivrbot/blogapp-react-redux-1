823800e3797c494ecb79b8d8242c455c56e9bc70
Shelly Tang <shellytang@gmail.com>
2017-08-22 18:38:53 -0700

    import router, added reducer and style starter files

--- src/index.js
+++ src/index.js
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import {BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
+class Hello extends React.Component {
+  render () { return <div>Hello!</div>}
+}
+
+class Goodbye extends React.Component {
+  render () { return <div>Bye!</div>}
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+      <BrowserRouter>
+        <div>
+          <Route path="/hello" component={Hello}/>
+          <Route path="/goodbye" component={Goodbye}/>
+        </div>
+      </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
