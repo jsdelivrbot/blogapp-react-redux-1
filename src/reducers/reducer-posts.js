5be7b003ed0b9fe8fb7e7ddae08010795ad5c79a
Shelly Tang <shellytang@gmail.com>
2017-09-04 15:51:46 -0700

    lodash notes

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -4,6 +4,7 @@ import {FETCH_POSTS} from '../actions';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
+    // _.mapKeys(array, property you want to pull from each object to use as a key of the resulting object)
       return _.mapKeys(action.payload.data, 'id');
     default: return state;
   }
