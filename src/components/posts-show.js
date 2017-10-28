135dc410de24f348481f04538d60b5b10c5e0c67
Shelly Tang <shellytang@gmail.com>
2017-10-28 13:39:23 -0700

    connected component

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+      // this.props.match.params.id;  // provided by react router
+      const {id} = this.props.match.params;
+      this.props.fetchPost(id);
+  }
 
+  // helperFunction() {
+  //   this.props.posts[this.props.match.params.id];
+  // }
 
   render() {
+
     return (
       <div>Posts Show!</div>
     )
@@ -11,4 +22,8 @@ class PostsShow extends Component {
 
  }
 
-export default PostsShow;
+// first argument is state (we only want the posts property), second argument is the props object this.props === ownProps; return single posts we want
+function mapStateToProps({posts}, ownProps) {
+  return { posts: posts[ownProps.match.params.id]};
+}
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
