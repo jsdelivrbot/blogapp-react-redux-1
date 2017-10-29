887567168a421db5cdb9030865eef1585441ef82
Shelly Tang <shellytang@gmail.com>
2017-10-28 17:44:09 -0700

    added notes

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -5,11 +5,7 @@ export default function(state = {}, action) {
   switch (action.type) {
 
     case FETCH_POST:
-    // const post = action.payload.data;
-    // return newState = {...state} // take existing posts we have and put them into a new object and add in a new key-value pair of fetched post
-    // newState[post.id] = post;
-    // return newState;
-
+    // take existing posts we have and put them into a new object and add in a new key-value pair of fetched post
       return { ...state, [action.payload.data.id]: action.payload.data }; // key interpolation; make a new key on object using value in [] and set value
     case FETCH_POSTS:
     // _.mapKeys(array, property you want to pull from each object to use as a key of the resulting object)
